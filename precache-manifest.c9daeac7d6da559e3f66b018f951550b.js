c413d67e3b43fb634183a3ac0a452f7ec0339a1c
Dipak Kumar Paul <pauldipakkr@gmail.com>
2022-10-17 11:53:45 +0530

    Updates

---
+++ precache-manifest.c9daeac7d6da559e3f66b018f951550b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "abd4165fbc8fd3bd6a4e",
+    "url": "/React-portfolio/static/css/main.dfbadf72.chunk.css"
+  },
+  {
+    "revision": "abd4165fbc8fd3bd6a4e",
+    "url": "/React-portfolio/static/js/main.abd4165f.chunk.js"
+  },
+  {
+    "revision": "0f15377751036204424c",
+    "url": "/React-portfolio/static/js/1.0f153777.chunk.js"
+  },
+  {
+    "revision": "9e83d492f2a76d1f26ae",
+    "url": "/React-portfolio/static/js/2.9e83d492.chunk.js"
+  },
+  {
+    "revision": "0740f279fa9de340d695",
+    "url": "/React-portfolio/static/js/runtime~main.0740f279.js"
+  },
+  {
+    "revision": "ced9e0dfe9615908ad0a064a63185744",
+    "url": "/React-portfolio/static/media/cv.ced9e0df.pdf"
+  },
+  {
+    "revision": "b5a3d0306401c1cdad0cc74b0d03a314",
+    "url": "/React-portfolio/static/media/dipakimg3.b5a3d030.png"
+  },
+  {
+    "revision": "3e3afb49b78fc1d56507e588798fa7ee",
+    "url": "/React-portfolio/static/media/dipakimg.3e3afb49.jpg"
+  },
+  {
+    "revision": "26e8db86f101cc289bae818503366115",
+    "url": "/React-portfolio/static/media/contempory.26e8db86.png"
+  },
+  {
+    "revision": "4ad35de3f76fbf5cdc9c245464e56b91",
+    "url": "/React-portfolio/static/media/uma.4ad35de3.png"
+  },
+  {
+    "revision": "2c2b146fbdb3420bd8e40d7cebac2ac9",
+    "url": "/React-portfolio/static/media/raga.2c2b146f.png"
+  },
+  {
+    "revision": "3773f1ad000e2cf8e74bbccdde1b359c",
+    "url": "/React-portfolio/static/media/sibarita.3773f1ad.png"
+  },
+  {
+    "revision": "cc66a326fae76de85675107e4f7db882",
+    "url": "/React-portfolio/index.html"
+  }
+];
