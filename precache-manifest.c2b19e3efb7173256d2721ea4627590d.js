799b91f495a2d4502e2cfafec9e10dc084b22abe
Dipak Kumar Paul <pauldipakkr@gmail.com>
2022-12-24 20:39:46 +0530

    Updates

---
+++ precache-manifest.c2b19e3efb7173256d2721ea4627590d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "96c7924d21650468a318",
+    "url": "/React-portfolio/static/css/main.bf2d1735.chunk.css"
+  },
+  {
+    "revision": "96c7924d21650468a318",
+    "url": "/React-portfolio/static/js/main.96c7924d.chunk.js"
+  },
+  {
+    "revision": "45b9bf53bf0b4c33a19d",
+    "url": "/React-portfolio/static/js/1.45b9bf53.chunk.js"
+  },
+  {
+    "revision": "607c0a46bb1796288999",
+    "url": "/React-portfolio/static/js/2.607c0a46.chunk.js"
+  },
+  {
+    "revision": "dbe7677d538e739b9392",
+    "url": "/React-portfolio/static/js/runtime~main.dbe7677d.js"
+  },
+  {
+    "revision": "2b6692cb6fe11485c06fa4888f9af5bd",
+    "url": "/React-portfolio/static/media/cv.2b6692cb.pdf"
+  },
+  {
+    "revision": "b5a3d0306401c1cdad0cc74b0d03a314",
+    "url": "/React-portfolio/static/media/dipakimg3.b5a3d030.png"
+  },
+  {
+    "revision": "3e3afb49b78fc1d56507e588798fa7ee",
+    "url": "/React-portfolio/static/media/dipakimg.3e3afb49.jpg"
+  },
+  {
+    "revision": "26e8db86f101cc289bae818503366115",
+    "url": "/React-portfolio/static/media/contempory.26e8db86.png"
+  },
+  {
+    "revision": "4ad35de3f76fbf5cdc9c245464e56b91",
+    "url": "/React-portfolio/static/media/uma.4ad35de3.png"
+  },
+  {
+    "revision": "2c2b146fbdb3420bd8e40d7cebac2ac9",
+    "url": "/React-portfolio/static/media/raga.2c2b146f.png"
+  },
+  {
+    "revision": "3773f1ad000e2cf8e74bbccdde1b359c",
+    "url": "/React-portfolio/static/media/sibarita.3773f1ad.png"
+  },
+  {
+    "revision": "f02a7c9fa721a6641249e1e192f75be7",
+    "url": "/React-portfolio/static/media/bmi.f02a7c9f.png"
+  },
+  {
+    "revision": "3c36efb7a8a03e0e29568df260864169",
+    "url": "/React-portfolio/static/media/movie.3c36efb7.png"
+  },
+  {
+    "revision": "2ebcc2db846538fd1dc9bb64d32728b5",
+    "url": "/React-portfolio/static/media/weather.2ebcc2db.png"
+  },
+  {
+    "revision": "b4f560a428d7993e1f67dcb4f2655f50",
+    "url": "/React-portfolio/index.html"
+  }
+];
